17cf6a330a33678fc579c99b01d66e3eefae4e70
zeus3droid <zeus3droid@gmail.com>
2014-02-14 13:15:57 +0200

    interface setup

--- gen/com/zcat/ingress_links/R.java
+++ gen/com/zcat/ingress_links/R.java
@@ -8,6 +8,9 @@
 package com.zcat.ingress_links;
 
 public final class R {
+    public static final class array {
+        public static final int linkAmpSPNRL=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,21 +27,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int distanceLBL=0x7f080000;
-        public static final int distanceTXTF=0x7f080001;
+        public static final int action_settings=0x7f090007;
+        public static final int aplETXT=0x7f090005;
+        public static final int aplLBL=0x7f090003;
+        public static final int calcBTN=0x7f090006;
+        public static final int distanceETXT=0x7f090001;
+        public static final int distanceLBL=0x7f090000;
+        public static final int linkAmpLBL=0x7f090002;
+        public static final int linkAmpSPNR=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int aplLBLTXT=0x7f050005;
         public static final int app_name=0x7f050000;
+        public static final int calcBTNTXT=0x7f050006;
         public static final int distanceLBLTXT=0x7f050002;
         public static final int distanceTXTF=0x7f050003;
+        public static final int linkAmpLBLTXT=0x7f050004;
     }
     public static final class style {
         /** 
@@ -62,10 +73,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
